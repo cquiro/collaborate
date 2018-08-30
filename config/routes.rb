22caa11b5b617e6258a572ad3e09364725fcfec0
Carlos Quiroga <cquiro@gmail.com>
2018-08-30 16:59:56 -0300

    Add URI /signup to routes for user registration.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: :sessions, controllers: { registrations: 'registrations' }
+  devise_for :users, skip: [:sessions], controllers: { registrations: 'registrations' }
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new', as: :new_user_session
     post 'login', to: 'devise/sessions#create', as: :user_session
     delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+
+    get 'signup', to: 'devise/registrations#new'
   end
 
   root to: 'pages#index'
