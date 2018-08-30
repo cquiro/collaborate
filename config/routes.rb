a2a671e8c62e3f392b160161b371c66846781526
Carlos Quiroga <cquiro@gmail.com>
2018-08-30 16:14:56 -0300

    Replace devise sessions routes with custom routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: :sessions
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
   root to: 'pages#index'
 end
