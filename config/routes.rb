bc0e3e54411f779d06afea0083a2cf5bd33f3a11
Carlos Quiroga <cquiro@gmail.com>
2018-09-05 20:05:33 -0300

    Add routes for registrations.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:sessions], controllers: { registrations: 'registrations' }
+   devise_for :users, skip: [:sessions, :registrations], controllers: { registrations: 'registrations' }
 
-  devise_scope :user do
-    get 'login', to: 'devise/sessions#new', as: :new_user_session
-    post 'login', to: 'devise/sessions#create', as: :user_session
-    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+   devise_scope :user do
+     get 'login', to: 'devise/sessions#new'
+     post 'login', to: 'devise/sessions#create', as: :user_session
+     delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
 
-    get 'signup', to: 'devise/registrations#new'
-  end
+     get 'cancel', to: 'registrations#cancel', as: :cancel_user_registration
+     get 'signup', to: 'registrations#new'
+     get 'edit', to: 'registrations#edit', as: :edit_user_registration
+     resource :users, as: :user_registration,
+                      controller: :registrations, except: [:show, :new, :edit]
+   end
 
-  root to: 'pages#index'
+   root to: 'pages#index'
 end
